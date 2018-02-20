71fd56fb11304902fe8b12240e40619a27d7157a
dledge <dledger@gmail.com>
2018-02-20 13:08:48 -0500

    small fixes

--- md2doku.py
+++ md2doku.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import argparse
-import sys
-import re
-def convertfile( mdTxt ):
+# This is a VERY bare bones converter from markdown to doku wiki.
+#
+# Usage:
+#   python md2doku.py inputfile.md
+# Output will be written to the console so it can be copied and pasted into a dokuwiki
 
-    #print(mdTxt)
-    # Headers
-    mdTxt = re.sub(r'##### (.*?) #####',r'== \1 ==',mdTxt)
-    mdTxt = re.sub(r'##### (.*?)\n',    r'== \1 ==',mdTxt)
-
-    mdTxt = re.sub(r'#### (.*?) ####',  r'=== \1 ===',mdTxt)
-    mdTxt = re.sub(r'#### (.*?)\n',     r'=== \1 ===',mdTxt)
-    mdTxt = re.sub(r'### (.*?) ###',    r'==== \1 ====',mdTxt)
-    mdTxt = re.sub(r'### (.*?)\n',      r'==== \1 ====',mdTxt)
-    mdTxt = re.sub(r'## (.*?) ##',      r'===== \1 =====',mdTxt)
-    mdTxt = re.sub(r'# (.*?) #',        r'====== \1 ======',mdTxt)
-    mdTxt = re.sub(r'# (.*?) \n',       r'====== \1 ======',mdTxt)
-
-    # code
-    mdTxt = re.sub(r'```C',   r'<code>',mdTxt)
-    mdTxt = re.sub(r'```',   r'</code>',mdTxt)
-    mdTxt = re.sub(r' `', r' //',mdTxt)
-    mdTxt = re.sub(r'` ', r'// ',mdTxt)
-    mdTxt = re.sub(r'([^A-Za-z0-9])`', r'\1//',mdTxt)
-    mdTxt = re.sub(r'`([^A-Za-z0-9])', r'//\1',mdTxt)
-
-    # images
-    mdTxt = re.sub(r'\!\[(.*?)\]\((.*?)\)',  r'{{\2\|\1}}',mdTxt)
-
-    print(mdTxt)
-
-    return
-
+import argparse, sys, re
 
 # Command line arguments
 parser = argparse.ArgumentParser(description='Convert markdown (md) to dokuwiki')
 parser.add_argument('infile', type=argparse.FileType('r'), help='Input markdown file')
 
+# If no arguments provided, print help
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
+
+# Parse arguments
 args = parser.parse_args()
 
-if args.infile:
-    convertfile(args.infile.read())
-else:
-    parser.print_help()
+# Get markdown text from input file
+mdTxt = args.infile.read()
+
+# Process headers
+mdTxt = re.sub(r'###### (.*?) ######', r'= \1=',mdTxt)
+mdTxt = re.sub(r'###### (.*?)\n',      r'= \1 =',mdTxt)
+mdTxt = re.sub(r'##### (.*?) #####',   r'== \1 ==',mdTxt)
+mdTxt = re.sub(r'##### (.*?)\n',       r'== \1 ==',mdTxt)
+mdTxt = re.sub(r'#### (.*?) ####',     r'=== \1 ===',mdTxt)
+mdTxt = re.sub(r'#### (.*?)\n',        r'=== \1 ===',mdTxt)
+mdTxt = re.sub(r'### (.*?) ###',       r'==== \1 ====',mdTxt)
+mdTxt = re.sub(r'### (.*?)\n',         r'==== \1 ====',mdTxt)
+mdTxt = re.sub(r'## (.*?) ##',         r'===== \1 =====',mdTxt)
+mdTxt = re.sub(r'## (.*?)\n',          r'===== \1 =====',mdTxt)
+mdTxt = re.sub(r'# (.*?) #',           r'====== \1 ======',mdTxt)
+mdTxt = re.sub(r'# (.*?)\n',           r'====== \1 ======',mdTxt)
+
+# Process code
+mdTxt = re.sub(r'```(C|javascript|ruby|c|python)',   r'<code>',mdTxt)
+mdTxt = re.sub(r'```',   r'</code>',mdTxt)
+mdTxt = re.sub(r' `', r' //',mdTxt)
+mdTxt = re.sub(r'` ', r'// ',mdTxt)
+mdTxt = re.sub(r'([^A-Za-z0-9])`', r'\1//',mdTxt)
+mdTxt = re.sub(r'`([^A-Za-z0-9])', r'//\1',mdTxt)
+
+# images
+mdTxt = re.sub(r'\!\[(.*?)\]\((.*?)\)',  r'{{\2\|\1}}',mdTxt)
+
+# bold
+#mdTxt = re.sub(r'(__)(.+?)(__)', r'**\2**',mdTxt)
+
+# italics
+mdTxt = re.sub(r'([^\*])\*{1}([^\*].*?[^\*])\*{1}([^\*])', r'\1//\2//\3',mdTxt)
+#mdTxt = re.sub(r'([^_])_(.+?)_([^_])', r'\1//\2//\3',mdTxt)
+
+print(mdTxt)
